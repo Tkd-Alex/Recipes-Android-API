1c581ff48aefbd7765da6869c283b309d92e5ba8
Tkd-Alex <alex.tkd.alex@gmail.com>
2017-12-18 16:16:12 +0100

    Init with empty maven project

---
+++ src/main/java/com/github/raa/recipes/android/api/App.java
@@ -0,0 +1,13 @@
+package com.github.raa.recipes.android.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
