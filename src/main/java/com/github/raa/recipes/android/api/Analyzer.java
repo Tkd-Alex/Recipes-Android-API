b5f7b9367ffe25835d4868902b20643a50222c99
Tkd-Alex <alex.tkd.alex@gmail.com>
2017-12-18 18:25:07 +0100

    Print all comment in each modificaion and print the method named

--- src/main/java/com/github/raa/recipes/android/api/Analyzer.java
+++ src/main/java/com/github/raa/recipes/android/api/Analyzer.java
@@ -1,22 +1,43 @@
 package com.github.raa.recipes.android.api;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.repodriller.domain.Commit;
 import org.repodriller.domain.Modification;
 import org.repodriller.persistence.PersistenceMechanism;
 import org.repodriller.scm.CommitVisitor;
 import org.repodriller.scm.SCMRepository;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.comments.Comment;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+
 public class Analyzer implements CommitVisitor {
 
 	@Override
 	public void process(SCMRepository repo, Commit commit, PersistenceMechanism writer) {
 		for(Modification m: commit.getModifications()) {
-			String []lines = m.getDiff().split("\n");
-			for(String line: lines) {
-				System.out.println(commit.getHash());
-				System.out.println(line);
-				writer.write(commit.getHash(), line);
+			CompilationUnit cu = JavaParser.parse(m.getSourceCode());
+			Optional<ClassOrInterfaceDeclaration> classStore = cu.getClassByName("AppStoreApp");
+			VoidVisitor<?> methodNameVisitor = new MethodNamePrinter();
+			methodNameVisitor.visit(cu,  null);
+			if(classStore.isPresent()) { 
+				//System.out.println(classStore.toString());
+				
 			}
+			/*
+			List<Comment> comments = cu.getAllContainedComments();
+			for(Comment c: comments) {
+				writer.write(
+						commit.getHash(), 
+						m.getFileName(), 
+						c.toString().replaceAll("\n", "")
+					);
+			}
+			*/
 		}
 	}
 
